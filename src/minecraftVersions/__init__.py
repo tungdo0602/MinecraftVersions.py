fc0aa277baad4affff30a88670bae14965e3d8d2
Tung <89498812+tungdo0602@users.noreply.github.com>
2023-03-30 18:23:39 +0700

    Update __init__.py

--- src/minecraftVersions/__init__.py
+++ src/minecraftVersions/__init__.py
@@ -7,4 +7,9 @@ A Python wrapper for Minecraft version.
 Author: tungdo0602
 """
 
+__title__ = "minecraftVersions"
+__version__ = "1.0.3"
+__author__ = "tungdo0602"
+__license__ = "MIT"
+
 from .utils import *

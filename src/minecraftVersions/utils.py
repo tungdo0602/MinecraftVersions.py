48f9fe9b2a310fab6aea6b73524bbfd52649bae2
Tung <89498812+tungdo0602@users.noreply.github.com>
2023-04-02 19:11:17 +0700

    alias + search version by string

--- src/minecraftVersions/utils.py
+++ src/minecraftVersions/utils.py
@@ -2,15 +2,18 @@ import requests
 from urllib.parse import urlparse, unquote
 
 class MCVersion():
-    def __init__(self, src=None):
-        data = requests.get(src if src else "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json")
-        if data.status_code == 200:
-            self.data = data.json()
+    def __init__(self, src=None, isRes=True):
+        if isRes:
+            data = requests.get(src if src else "https://launchermeta.mojang.com/mc/game/version_manifest_v2.json")
+            if data.status_code == 200:
+                self.data = data.json()
+            else:
+                raise Exception("Failed to get data from the server!")
         else:
-            raise Exception("Failed to get data from the server!")
+            self.data = src
         
     def latest(self):
-        return latest(self.data["latest"], self.data["versions"])
+        return latest(self.data)
     
     def getVersion(self, version: str):
         for v in self.data["versions"]:
@@ -18,31 +21,95 @@ class MCVersion():
                 return versionManager(v)
         raise ValueError("Could not find that Minecraft version!")
     
+    def get(self, version: str):
+        return self.getVersion(version)
+    
+    def version(self, version: str):
+        return self.getVersion(version)
+    
+    def versionInclude(self, s, first=True):
+        versions = []
+        for v in self.data["versions"]:
+            if s in v["id"]:
+                if first:
+                    return v["id"]
+                else:
+                    versions.append(v["id"])
+        return versions
+    
+    def getVersionInclude(self, s, first=True):
+        return self.versionInclude(s, first)
+    
+    def getByInclude(self, s, first=True):
+        return self.versionInclude(s, first)
+    
     def getAllVersions(self):
+        """
         versions = []
         for v in self.data["versions"]:
             versions.append(v["id"])
-        return versions
+        """
+        return allVersions(self.data)
+    
+    def getAllVersion(self):
+        return allVersions(self.data)
+    
+    def getAll(self, s="", first=True):
+        if s:
+            return self.versionInclude(s, first)
+        else:
+            return allVersions(self.data)
+
+class allVersions():
+    def __init__(self, data):
+        self.data = data
+        
+    def __str__(self):
+        return " ".join([i["id"] for i in self.data["versions"]])
+    
+    def toList(self):
+        return [i["id"] for i in self.data["versions"]]
+    
+    def list(self):
+        return self.toList()
+    
+    def get(self, version):
+        return MCVersion(self.data, False).getVersion(version)
+    
+    def getVersion(self, version):
+        return MCVersion(self.data, False).getVersion(version)
+    
+    def version(self, version):
+        return MCVersion(self.data, False).getVersion(version)
+    
+    def versionIncludes(self, s, first=True):
+        return MCVersion(self.data, False).versionInclude(s, first)
+    
+    def getVersionInclude(self, s, first=True):
+        return MCVersion(self.data, False).versionInclude(s, first)
+    
+    def getByInclude(self, s, first=True):
+        return MCVersion(self.data, False).versionInclude(s, first)
+    
+    def getAll(self, s="", first=True):
+        if s:
+            MCVersion(self.data, False).versionInclude(s, first)
+        else:
+            return allVersions(self.data)
 
 class latest():
-    def __init__(self, data, vdata):
+    def __init__(self, data):
         self.data = data
-        self.vdata = vdata
-    
-    def getVersion(self, version: str):
-        for v in self.vdata:
-            if version == v["id"]:
-                return versionManager(v)
-        raise ValueError("Could not find that Minecraft version!")
     
     def release(self):
-        return self.getVersion(self.data["release"])
+        return MCVersion(self.data, False).getVersion(self.data["latest"]["release"])
     
     def snapshot(self):
-        return self.getVersion(self.data["snapshot"])
+        return MCVersion(self.data, False).getVersion(self.data["latest"]["snapshot"])
 
 class versionManager():
     def __init__(self, data):
+        self.data = data
         self.version = data["id"]
         self.type = data["type"]
         self.url = data["url"]
@@ -55,6 +122,9 @@ class versionManager():
             self.data = versionData.json()
         else:
             raise Exception("Failed to get data from mojang server!")
+    
+    def __str__(self):
+        return self.data["id"]
     
     def client(self):
         return downloadObj(self.data["downloads"]["client"])

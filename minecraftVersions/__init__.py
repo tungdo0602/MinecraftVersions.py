0e5ebf628ae9076f8a074eef745ecd91abbc57f7
Tung <89498812+tungdo0602@users.noreply.github.com>
2023-04-02 19:20:40 +0700

    fix directory

---
+++ minecraftVersions/__init__.py
@@ -0,0 +1,15 @@
+"""
+Minecraft Version Wrapper
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A simple Minecraft version wrapper written in Python
+
+Author: tungdo0602
+"""
+
+__title__ = "minecraftVersions"
+__version__ = "1.0.5.1"
+__author__ = "tungdo0602"
+__license__ = "MIT"
+
+from .utils import *
